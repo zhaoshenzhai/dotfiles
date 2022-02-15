43a76bda531297badb2315ac3822a6348ff71299
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-02-15 20:57:58 +0400

    Init wifi

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 xset r rate 250 &                                   # Set keyboard repeat rate
 xset b off &                                        # Remove beep
+nmcli dev wifi con Z-5GHz &                         # Wifi
 
 # Start applications
 dropbox &
