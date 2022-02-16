95a0680e34d0eb020733c2731b00defad867e471
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-02-16 14:53:36 +0400

    Updated init

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,9 +3,14 @@ xset r rate 250 &                                   # Set keyboard repeat rate
 xset b off &                                        # Remove beep
 nmcli dev wifi con Z-5GHz &                         # Wifi
 
-# Start applications
+# Start background applications
 dropbox &
 nitrogen --restore &
 
-#Open reminders
+# Open reminders
 alacritty --class reminders,reminders -e nvim ~/Reminders/self_study.md &
+
+# Start main applications
+google-chrome-stable --profile-directory=Default &
+obsidian &
+spotify &
