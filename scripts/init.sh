a03e99463d8ad130782a677d933fd2bf4e240f78
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-06-12 08:56:56 +0400

    Start chrome with tabs

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,6 +13,6 @@ alacritty --class reminders,reminders -e nvim ~/Dropbox/Reminders/Dates.md &
 zathura '~/Dropbox/University/Courses/22F/MATH133/Fall 2020.pdf' &
 
 # Start main applications
-google-chrome-stable --profile-directory=Default --force-dark-mode &
+google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/inbox https://github.com/zhaoshenzhai https://math.stackexchange.com https://mcgill.ca/exams/dates &
 obsidian &
 spotify &
