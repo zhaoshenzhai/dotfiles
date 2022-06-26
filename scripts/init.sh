01209fb18a044c5a4286b0e1a2a410c6f0a3cf3c
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-06-26 14:25:35 +0400

    Removed a chrome tab

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,6 +14,6 @@ alacritty --class reminders,reminders -e nvim ~/Dropbox/Reminders/Items.md &
 zathura '~/Dropbox/University/Courses/22F/MATH133/Fall 2020.pdf' &
 
 # Start main applications
-google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/inbox https://github.com/zhaoshenzhai https://math.stackexchange.com https://mcgill.ca/exams/dates &
+google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/inbox https://github.com/zhaoshenzhai https://math.stackexchange.com &
 obsidian &
 spotify &
