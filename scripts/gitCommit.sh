2144a9d90ec8af0523ca1ffe740187be0bc387f0
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-06-28 23:24:01 +0400

    Fixed templatesInsertLine

--- scripts/gitCommit.sh
+++ scripts/gitCommit.sh
@@ -77,7 +77,7 @@ if [ -z "$choice" ] || [ "$choice" == "Y" ]; then
         source ~/MathWiki/.scripts/stats.sh -r
 
         templatesInsertLine=$(grep -n "#### Templates" ~/.config/nvim/UltiSnips/markdown.snippets | sed 's/:.*$//g')
-        sed -i 's|\[templatesInsert\]:.*$|\[templatesInsert\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/UltiSnips/vimwiki.snippets#L'"$templatesInsertLine"'|g' README.md
+        sed -i 's|\[templatesInsert\]:.*$|\[templatesInsert\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/UltiSnips/markdown.snippets#L'"$templatesInsertLine"'|g' README.md
 
         tikzInsertLine=$(grep -n "newTikZ.sh" ~/.config/nvim/config/MathWiki.vim | sed 's/:.*$//g')
         sed -i 's|\[tikzInsert\]:.*$|\[tikzInsert\]: https://github.com/zhaoshenzhai/dotfiles/blob/master/nvim/config/MathWiki.vim#L'"$tikzInsertLine"'|g' README.md
