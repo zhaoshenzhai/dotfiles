2b3974d2ea171271eeea74605141d870ccaed640
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-06-29 09:30:37 +0400

    Fixed init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -15,10 +15,7 @@ alacritty --class reminders,reminders -e nvim ~/Dropbox/Reminders/Items.md &
 zathura '~/Dropbox/University/Courses/22F/MATH133/Fall 2020.pdf' &
 
 # Start main applications
-google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com &
-google-chrome-stable --profile-directory=Default --force-dark-mode https://mail.google.com/mail/u/0/#inbox &
-google-chrome-stable --profile-directory=Default --force-dark-mode https://outlook.office.com/mail/inbox &
-google-chrome-stable --profile-directory=Default --force-dark-mode https://github.com/zhaoshenzhai &
-google-chrome-stable --profile-directory=Default --force-dark-mode https://math.stackexchange.com &
 obsidian &
 spotify &
+sleep 20
+google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/inbox https://github.com/zhaoshenzhai https://math.stackexchange.com &
