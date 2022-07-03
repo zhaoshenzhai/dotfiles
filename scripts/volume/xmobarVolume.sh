112cd0753616f2ed2d9a362bc52d1af8b9d738c9
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-07-03 21:42:24 +0400

    Fixed bluetooth battery in xmobarVolume.sh

--- scripts/volume/xmobarVolume.sh
+++ scripts/volume/xmobarVolume.sh
@@ -17,7 +17,13 @@ elif [ "$LINES" -eq 3 ]; then
     SINK=`echo "$RAW" | sed -n 3p`
     HEADPHONE=${SINK%%\ *}
     MUTE=`pamixer --sink "$HEADPHONE" --get-mute`
-    HEADPHONE_BAT=`bluetoothctl info | tail -n1 | sed -E 's/(.*\()//g' | sed 's/.$//'`
+    BLUETOOTHINFO=`bluetoothctl info | tail -n1`
+    if [[ `echo "$BLUETOOTHINFO" | cut -c2` == "B" ]]; then
+        HEADPHONE_BAT=`echo "$BLUETOOTHINFO" | sed -E 's/(.*\()//g' | sed 's/.$//'`
+    else
+        HEADPHONE_BAT="--"
+    fi
+
     if [ "$MUTE" = true ]; then
         printf "%s %s | %s" "<fn=2> </fn>" "Muted" $HEADPHONE_BAT"%"
     else
