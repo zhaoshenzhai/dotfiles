83992a969292a3448c39f12d388a350c444788ff
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-07-04 18:15:18 +0400

    Added second bluetooth device

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -5,10 +5,12 @@ while [ ! -z "$1" ]; do
         --connect|-c)
             shift
             `bluetoothctl connect 88:D0:39:9F:C9:4B`
+            `bluetoothctl connect E8:07:BF:CB:CD:1A`
             ;;
         --disconnect|-d)
             shift
             `bluetoothctl disconnect 88:D0:39:9F:C9:4B`
+            `bluetoothctl disconnect E8:07:BF:CB:CD:1A`
             `playerctl -p spotify pause`
             ;;
     esac
