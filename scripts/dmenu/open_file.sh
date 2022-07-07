4d2c94de2bcb5fb0f14e3b8fc2a106eb6424517c
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2022-07-07 12:49:11 +0400

    Fixed dmenu open_file.sh

--- scripts/dmenu/open_file.sh
+++ scripts/dmenu/open_file.sh
@@ -137,7 +137,7 @@ case $main_choice in
         fi
     ;;
     "Reminders")
-        root_path="$HOME/Dropbox/Reminders/"
+        root_path="$HOME/Dropbox/Misc/Reminders/"
 
         choice=$(find $root_path -type f | cut -c$((${#root_path}+1))- | dmenu -i -p 'Open:' $lines $colors -fn 'courier prime:spacing=1:pixelsize=20')
 
