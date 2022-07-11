5a1e9ee996f8fd69b433b5ff2d50c5521828f8c7
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-07-11 10:32:04 +0400

    Decluttering

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-xset r rate 300 &                                   # Set keyboard repeat rate
-xset b off &                                        # Remove beep
-xsetroot -cursor_name left_ptr                      # Cursor
-nmcli dev wifi con Z &                              # Wifi
+xset r rate 300 &                # Set keyboard repeat rate
+xset b off &                     # Remove beep
+xsetroot -cursor_name left_ptr   # Cursor
+nmcli dev wifi con Z &           # Wifi
 
 # Start background applications
 dropbox &
@@ -20,5 +20,3 @@ obsidian &
 sleep 10
 spotify &
 sleep 10
-
-#google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/inbox https://github.com/zhaoshenzhai https://math.stackexchange.com &
