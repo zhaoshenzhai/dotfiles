958f7084d0026c28c98268f176b215059f8cc0e3
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-07-11 06:13:27 +0400

    Bluetooth

--- scripts/bluetooth.sh
+++ scripts/bluetooth.sh
@@ -4,20 +4,24 @@ while [ ! -z "$1" ]; do
     case "$1" in
         --connect1|-c1)
             shift
+            `bluetoothctl power on`
             `bluetoothctl connect 88:D0:39:9F:C9:4B`
             ;;
         --connect2|-c2)
             shift
+            `bluetoothctl power on`
             `bluetoothctl connect E8:07:BF:CB:CD:1A`
             ;;
         --disconnect1|-d1)
             shift
             `bluetoothctl disconnect 88:D0:39:9F:C9:4B`
+            `bluetoothctl power off`
             `playerctl -p spotify pause`
             ;;
         --disconnect2|-d2)
             shift
             `bluetoothctl disconnect E8:07:BF:CB:CD:1A`
+            `bluetoothctl power off`
             `playerctl -p spotify pause`
             ;;
     esac
--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,6 @@ xset r rate 300 &                                   # Set keyboard repeat rate
 xset b off &                                        # Remove beep
 xsetroot -cursor_name left_ptr                      # Cursor
 nmcli dev wifi con Z &                              # Wifi
-bluetoothctl power on &                             # Bluetooth
 
 # Start background applications
 dropbox &
@@ -16,7 +15,10 @@ alacritty --class reminders,reminders -e nvim ~/Dropbox/Others/Reminders/Items.m
 zathura '~/Dropbox/University/Courses/22F/MATH133/Syllabus/Fall 2020.pdf' &
 
 # Start main applications
+sleep 10
 obsidian &
-sleep 20
+sleep 10
 spotify &
+sleep 10
+
 #google-chrome-stable --profile-directory=Default --force-dark-mode https://www.youtube.com https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/inbox https://github.com/zhaoshenzhai https://math.stackexchange.com &
