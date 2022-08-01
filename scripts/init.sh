287fc60c0a72095fb8630cd95204422304881da5
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-08-01 00:30:39 +0000

    Removed `sleep`

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,9 +14,6 @@ alacritty --class reminders,reminders -e nvim ~/Dropbox/Others/Reminders/Items.m
 zathura '~/Dropbox/University/Courses/22F/MATH133/Syllabus/Fall 2020.pdf' &
 
 # Start main applications
-sleep 10
 obsidian &
-sleep 10
 spotify &
-sleep 10
 qutebrowser-profile --new 'Z' https://www.youtube.com https://www.github.com/zhaoshenzhai https://mail.google.com/mail/u/0/#inbox https://outlook.office.com/mail/ &
