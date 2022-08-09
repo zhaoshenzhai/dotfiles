0e2c472fafaf7bdbfe4dec0d72f75aef3a414a7d
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-08-09 16:42:08 +0000

    Added chromium

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -45,7 +45,9 @@ import XMonad.Util.WorkspaceCompare
 
 -- Keys
 import Graphics.X11.ExtraTypes.XF86
+
 ---------------------------------------------------------------------------------------------------------------------
+
 myTerminal                                   = "alacritty"                       :: String
 myBorderWidth                                = 2                                 :: Dimension
 myWindowGap                                  = 0                                 :: Integer
@@ -54,7 +56,9 @@ myFocusedBorderColor                         = "#5C6370"                        
 myUnFocusedBorderColor                       = "#1E2127"                         :: String
 myFocusFollowsMouse                          = True                              :: Bool
 myClickJustFocuses                           = False                             :: Bool
+
 ---------------------------------------------------------------------------------------------------------------------
+
 mySpacing :: Integer -> l a -> ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border 0 i 0 i) True (Border i 0 i 0) True
 
@@ -75,7 +79,9 @@ myLayoutHook =
     where
         -- myLayout = full ||| tall ||| grid
         myLayout = full ||| tall
+
 ---------------------------------------------------------------------------------------------------------------------
+
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [
         -- Navigation
@@ -86,8 +92,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         -- Browser
         ((modm, xK_w              ), spawn "./.config/scripts/openQute.sh -Z"),
         ((modm .|. shiftMask, xK_w), spawn "./.config/scripts/openQute.sh -P"),
+        ((modm, xK_g              ), spawn "chromium --force-dark-mode"      ),
 
-        -- My scripts
+        -- Scripts
         ((modm .|. shiftMask, xK_s), spawn "alacritty -e ~/.config/scripts/stopwatch.sh"     ),
         ((modm .|. shiftMask, xK_g), spawn "alacritty -e ~/.config/scripts/gitCommit.sh"     ),
         ((modm .|. shiftMask, xK_m), spawn "alacritty -e ~/Dropbox/MathWiki/.scripts/main.sh"),
@@ -99,7 +106,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         -- GitHub
         ((controlMask .|. shiftMask, xK_g), spawn "xclip -sel clip ~/.config/.gitpat"),
 
-        -- Window management
+        -- Windows
         ((modm, xK_f     ), sendMessage NextLayout  ),
         ((modm, xK_grave ), sendMessage ToggleStruts),
         ((modm, xK_Tab   ), windows W.focusDown     ),
@@ -114,7 +121,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         ((modm, xK_F7), spawn "lux -s 10%"),
         ((modm, xK_F8), spawn "lux -a 10%"),
 
-        -- Audio control
+        -- Audio
         ((modm, xK_F1              ), spawn "./.config/scripts/audioControl.sh -t"  ),
         ((modm, xK_F2              ), spawn "./.config/scripts/audioControl.sh -d 5"),
         ((modm, xK_F3              ), spawn "./.config/scripts/audioControl.sh -i 5"),
@@ -128,19 +135,21 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         ((modm, xK_d               ), spawn "./.config/scripts/audioControl.sh -d1" ),
         ((modm .|. shiftMask, xK_d ), spawn "./.config/scripts/audioControl.sh -d2" ),
 
-        -- System info
-        ((modm, xK_h), spawn "alacritty -e htop"),
+        -- Info
+        ((modm, xK_h), spawn "alacritty --class sys,sys -e htop"),
         ((modm, xK_p), spawn "pavucontrol"      ),
 
         -- Xmonad
-        ((modm .|. shiftMask, xK_q     ), spawn "xmonad --recompile; killall xmobar; xmonad --restart"),
-        ((modm .|. shiftMask, xK_Delete), io exitSuccess)
+        ((modm .|. shiftMask, xK_r), spawn "xmonad --recompile; killall xmobar; xmonad --restart"),
+        ((modm .|. shiftMask, xK_q), io exitSuccess)
     ]
 
     ++[((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+
 ---------------------------------------------------------------------------------------------------------------------
+
 myWorkspaces = [
     "<fn=2>\xf303  </fn>", -- Arch
     "<fn=2>\xf269  </fn>", -- Browser
@@ -153,15 +162,20 @@ myWorkspaces = [
     ]
 
 myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..]
+
 ---------------------------------------------------------------------------------------------------------------------
+
 myStartupHook = do
     spawnOnce "~/.config/scripts/init.sh &"
+
 ---------------------------------------------------------------------------------------------------------------------
+
 myManageHook = composeAll
     [
         className =? "reminders"     --> viewShift (myWorkspaces !! 0),
         className =? "discord"       --> viewShift (myWorkspaces !! 0),
         className =? "qutebrowser"   --> viewShift (myWorkspaces !! 1),
+        className =? "Chromium"      --> viewShift (myWorkspaces !! 1),
         className =? "obsidian"      --> viewShift (myWorkspaces !! 2),
         className =? "nvim"          --> viewShift (myWorkspaces !! 2),
         className =? "mpv"           --> viewShift (myWorkspaces !! 5),
@@ -172,7 +186,9 @@ myManageHook = composeAll
     ]
 
     where viewShift = doF . liftM2 (.) W.greedyView W.shift
+
 ---------------------------------------------------------------------------------------------------------------------
+
 main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar -x 0 /home/zhao/.config/xmonad/xmobarrc"
