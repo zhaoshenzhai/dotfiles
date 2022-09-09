44806529ff00e4f40065cfe1f86850e3d361a073
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-09-09 14:16:48 -0400

    Custom title for assignments

--- scripts/newAssignment.sh
+++ scripts/newAssignment.sh
@@ -8,7 +8,12 @@ if [[ ! -z "$1" ]]; then
 
     courseName=$(echo $PWD | grep -Po "[A-Z]{4}[1-9]{3}.*/" | sed 's/\/.*//g' | sed 's/_/\ /g' | sed '0,/\ /{s/\ /\ -\ /}')
     termYear=$(echo $PWD | grep -Po "Y\d_.*/" | sed 's/\/.*//g' | sed 's/^.*_//g')\ $(echo $PWD | grep -Po "20\d\d")
-    title=$(echo $1 | sed 's/_/\ /g')
+
+    if [[ ! -z "$2" ]]; then
+        title=$2
+    else
+        title=$(echo $1 | sed 's/_/\ /g')
+    fi
 
     sed -i 's/COURSE_NAME/'"$courseName"'/g' $1.tex
     sed -i 's/TERM_YEAR/'"$termYear"'/g' $1.tex
