89393c0f5c523bda8ce01772fb3524073c98fdaa
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-09-22 16:46:02 -0400

    Templates

--- scripts/newAssignment.sh
+++ scripts/newAssignment.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 file=""
+template="template.tex"
 courseName=""
 termYear=""
 displayedTitle=""
 dueDate="DUE_DATE"
 
-while getopts 'f:t:d:' OPTION; do
+while getopts 'f:t:d:s' OPTION; do
     case "$OPTION" in
         f)
             file=$OPTARG
             mkdir $file
             cd $file
 
-            cp -r $DOTFILES_DIR/files/assignmentsTemplate/* $PWD
-            mv template.tex $file.tex
+            cp -r $DOTFILES_DIR/files/assignmentsTemplate/*.sty $PWD
+            cp -r $DOTFILES_DIR/files/assignmentsTemplate/$template $PWD
+            mv $template $file.tex
 
             courseName=$(echo $PWD | grep -Po "[A-Z]{4}[1-9]{3}.*/" | sed 's/\/.*//g' | sed 's/_/\ /g' | sed '0,/\ /{s/\ /\ -\ /}')
             termYear=$(echo $PWD | grep -Po "Y\d_.*/" | sed 's/\/.*//g' | sed 's/^.*_//g')\ $(echo $PWD | grep -Po "20\d\d")
@@ -28,8 +30,11 @@ while getopts 'f:t:d:' OPTION; do
             sed -i 's/TITLE/'"$displayedTitle"'/g' $file.tex
         ;;
         d)
-            dueDate=$(echo $OPTARG | sed 's/\\/\\\\/g' | sed 's/\$/\\\$/g' | sed 's/\\t/\\\\\\t/g')
+            dueDate=$(echo $OPTARG | sed 's/\\/\\\\/g' | sed 's/\$/\\\$/g' | sed 's/\\t/\\t/g')
             sed -i 's/DUE_DATE/'"$dueDate"'/g' $file.tex
+        ;;
+        s)
+            template="template_s.tex"
         ;;
   esac
 done
