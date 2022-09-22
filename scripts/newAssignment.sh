37cc2e75e7fc1bdfe3c2b96c7911855849d932b8
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2022-09-22 13:13:44 -0400

    Improved `newAssignment.sh`

--- scripts/newAssignment.sh
+++ scripts/newAssignment.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-if [[ ! -z "$1" ]]; then
-    mkdir $1
-    cd $1
-    cp -r $DOTFILES_DIR/files/assignmentsTemplate/* $PWD
-    mv template.tex $1.tex
+file=""
+courseName=""
+termYear=""
+displayedTitle=""
+dueDate="DUE_DATE"
 
-    courseName=$(echo $PWD | grep -Po "[A-Z]{4}[1-9]{3}.*/" | sed 's/\/.*//g' | sed 's/_/\ /g' | sed '0,/\ /{s/\ /\ -\ /}')
-    termYear=$(echo $PWD | grep -Po "Y\d_.*/" | sed 's/\/.*//g' | sed 's/^.*_//g')\ $(echo $PWD | grep -Po "20\d\d")
+while getopts 'f:t:d:' OPTION; do
+    case "$OPTION" in
+        f)
+            file=$OPTARG
+            mkdir $file
+            cd $file
 
-    if [[ ! -z "$2" ]]; then
-        title=$2
-    else
-        title=$(echo $1 | sed 's/_/\ /g')
-    fi
+            cp -r $DOTFILES_DIR/files/assignmentsTemplate/* $PWD
+            mv template.tex $file.tex
 
-    sed -i 's/COURSE_NAME/'"$courseName"'/g' $1.tex
-    sed -i 's/TERM_YEAR/'"$termYear"'/g' $1.tex
-    sed -i 's/TITLE/'"$title"'/g' $1.tex
-fi
+            courseName=$(echo $PWD | grep -Po "[A-Z]{4}[1-9]{3}.*/" | sed 's/\/.*//g' | sed 's/_/\ /g' | sed '0,/\ /{s/\ /\ -\ /}')
+            termYear=$(echo $PWD | grep -Po "Y\d_.*/" | sed 's/\/.*//g' | sed 's/^.*_//g')\ $(echo $PWD | grep -Po "20\d\d")
+            displayedTitle=$(echo $file | sed 's/_/\ /g')
+
+            sed -i 's/COURSE_NAME/'"$courseName"'/g' $file.tex
+            sed -i 's/TERM_YEAR/'"$termYear"'/g' $file.tex
+        ;;
+        t)
+            displayedTitle=$OPTARG
+            sed -i 's/TITLE/'"$displayedTitle"'/g' $file.tex
+        ;;
+        d)
+            dueDate=$(echo $OPTARG | sed 's/\\/\\\\/g' | sed 's/\$/\\\$/g' | sed 's/\\t/\\\\\\t/g')
+            sed -i 's/DUE_DATE/'"$dueDate"'/g' $file.tex
+        ;;
+  esac
+done
+shift "$(($OPTIND -1))"
