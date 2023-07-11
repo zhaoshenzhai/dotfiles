7af00c107eb708b0f6ca0c2148ba8d3b380a77de
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2023-07-11 15:40:57 -0400

    Changed `compressed` to `tmp`

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -2,23 +2,23 @@
 
 for file in "$@"; do
     echo -ne "${YELLOW}Compressing: $file${NC}\r"
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$file.compressed" "$file"
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.5 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$file.tmp" "$file"
     
     oldSize=$(du "$file" | awk '{print $1}')
-    newSize=$(du "$file.compressed" | awk '{print $1}')
+    newSize=$(du "$file.tmp" | awk '{print $1}')
     
     echo -ne "\033[0K\r"
 
     if [[ $newSize -ge $oldSize ]]; then
-        cp "$file" "$file.compressed"
+        cp "$file" "$file.tmp"
         echo -e "${CYAN}[No changes] $file${NC}"
     else
         oldSizeHuman=$(du -h "$file" | awk '{print $1}')
-        newSizeHuman=$(du -h "$file.compressed" | awk '{print $1}')
+        newSizeHuman=$(du -h "$file.tmp" | awk '{print $1}')
        
         echo -e "${GREEN}[$oldSizeHuman  ->  $newSizeHuman] $file${NC}"
     fi
 
     mv "$file" "$file.bak"
-    mv "$file.compressed" "$file"
+    mv "$file.tmp" "$file"
 done
