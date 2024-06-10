f6b6f2636c2fa48b77c85caf2d3fc1e3abec4eb6
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2024-06-10 10:55:43 -0400

    `newLaTeX.sh`

---
+++ scripts/newLaTeX.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+filesPath=$DOTFILES_DIR/files/LaTeXTemplate
+name=
+
+# Help
+HELP() {
+    echo -e "Usage: ./newLaTeX.sh [-n name]"
+}
+
+# Input
+while [[ ! -z $1 ]]; do
+    case $1 in
+        -h|--help)
+            HELP
+            exit 0
+            ;;
+        -n)
+            name=$2
+    esac
+    shift
+    shift
+done
+
+# Validate
+if [[ -z $name ]]; then
+    echo -e "${RED}Error: Expected [-n] flag.${NC}"
+    HELP
+    exit 1
+fi
+
+mkdir $name
+cd $name
+
+cp $filesPath/* .
+mv file.tex $name.tex
