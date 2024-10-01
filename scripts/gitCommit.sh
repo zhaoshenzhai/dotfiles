217e14f71a94e7db76c35b75b51bb5004240668d
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2024-10-01 14:48:08 -0400

    removed SURA

--- scripts/gitCommit.sh
+++ scripts/gitCommit.sh
@@ -4,7 +4,6 @@ REPOS="
 MathWiki     $MATHWIKI_DIR
 Dotfiles     $DOTFILES_DIR
 Courses      $UNIVERSITY_DIR/Courses
-SURA24S      $UNIVERSITY_DIR/Courses/.old/SURA24S_Tree-like_Graphings_of_CBERs
 MathLinks    $MATHLINKS_DIR"
 
 REPOS=$(echo "$REPOS" | sed 1d)
@@ -23,16 +22,12 @@ GETSTATUS() {
     if [[ -z $1 ]]; then
         if [[ $repoName == "MathWiki" ]] || [[ $repoNum == 1 ]]; then
             echo $(git -c color.status=always status ':(exclude)docs/*' ':(exclude)Site/static/allFiles.json' 2>&1)
-        elif [[ $repoName == "SURA24S" || $repoNum == 4 ]]; then
-            echo $(git -c color.status=always status ':(exclude)*.pdf' 2>&1)
         else
             echo $(git -c color.status=always status 2>&1)
         fi
     else
         if [[ $repoName == "MathWiki" ]] || [[ $repoNum == 1 ]]; then
             echo $(git -c color.status=always status ':(exclude)docs/*' ':(exclude)Site/static/allFiles.json' | tee /dev/tty)
-        elif [[ $repoName == "SURA24S" || $repoNum == 4 ]]; then
-            echo $(git -c color.status=always status ':(exclude)*.pdf' | tee /dev/tty)
         else
             echo $(git -c color.status=always status | tee /dev/tty)
         fi
@@ -52,8 +47,6 @@ UPDATE() {
 SHOWDIFF() {
     if [[ $repoName == "MathWiki" ]] || [[ $repoNum == 1 ]]; then
         SHOWDIFFHELPER "docs/*" "Site/static/allFiles.json"
-    elif [[ $repoName == "SURA24S" || $repoNum == 4 ]]; then
-        SHOWDIFFHELPER "*.pdf"
     else
         SHOWDIFFHELPER
     fi
@@ -148,9 +141,6 @@ else
             cd $UNIVERSITY_DIR/Courses
         ;;
         "4")
-            cd $UNIVERSITY_DIR/Courses/.old/SURA24S_Tree-like_Graphings_of_CBERs
-        ;;
-        "5")
             cd $MATHLINKS_DIR
         ;;
         *)
