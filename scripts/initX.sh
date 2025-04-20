bf0ca2878ac8b615b1d012a0a68a963e01442a27
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2025-04-19 22:21:41 -0400

    init

--- scripts/initX.sh
+++ scripts/initX.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-sudo xremap /home/zhao/.config/xremap/config.yml
+sudo killall xremap
+sudo xremap /home/zhao/.config/xremap/config.yml &
+sleep 1
 xset r rate 150
 xset b off
 xsetroot -cursor_name left_ptr
