bc1fbbfe28bc4f72c5e79446287eb40a651df054
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2025-05-02 22:15:04 -0400

    mathwiki

--- scripts/openQute.sh
+++ scripts/openQute.sh
@@ -20,13 +20,10 @@ while [ ! -z "$1" ]; do
                 :'set -u localhost:1313 input.mode_override passthrough'\
                 :'set statusbar.show never'\
                 :'mode-enter passthrough'\
-                :'bind --mode=passthrough k scroll up'\
-                :'bind --mode=passthrough j scroll down'\
-                :'bind --mode=passthrough j scroll down'\
-                :'bind --mode=passthrough gg scroll top'\
-                :'bind --mode=passthrough G scroll bottom'\
-                :'bind --mode=passthrough u undo'\
-                :'bind --mode=passthrough f hint'\
+                :'bind --mode=passthrough <Ctrl+g> scroll top'\
+                :'bind --mode=passthrough <Ctrl+Shift+g> scroll bottom'\
+                :'bind --mode=passthrough <Ctrl+o> undo'\
+                :'bind --mode=passthrough <Ctrl+f> hint'\
                 :'bind --mode=passthrough <Ctrl+u> cmd-repeat 20 scroll up'\
                 :'bind --mode=passthrough <Ctrl+d> cmd-repeat 20 scroll down'\
                 :'bind --mode=passthrough <Ctrl+w> tab-close'\
