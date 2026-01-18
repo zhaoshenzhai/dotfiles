46c2c64890f19a4121b23b70bc88bfb13c96fcc0
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2026-01-18 11:22:08 -0500

    applications

--- scripts/initApplications.sh
+++ scripts/initApplications.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Reminders
-cd /home/zhao/Dropbox/Others/Reminders
-notes=$(find . -maxdepth 1 -type f | grep ".md")
-while IFS= read -r note; do
-    kitty --class reminders -e nvim "$note" &
-done <<< "$notes"
+# # Reminders
+# cd /home/zhao/Dropbox/Others/Reminders
+# notes=$(find . -maxdepth 1 -type f | grep ".md")
+# while IFS= read -r note; do
+#     kitty --class reminders -e nvim "$note" &
+# done <<< "$notes"
 
 # Applications
 dropbox &
 nitrogen --restore &
 qutebrowser-profile --load 'Z' &
-qutebrowser-profile --load 'P' &
-spotify &
+# qutebrowser-profile --load 'P' &
+# spotify &
