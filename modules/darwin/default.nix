544a7dcd619d622ad50ebffec38d5fb9cfff115c
Zhaoshen Zhai <zhao@puppy.local>
2026-01-31 03:39:53 -0500

    formatting

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,22 +1,19 @@
-{pkgs, ... }: {
-    # Nix stuff (do not touch!)
+{ pkgs, ... }: {
+    # --- System Core ---
     nixpkgs.hostPlatform = "aarch64-darwin";
     nix.extraOptions = "experimental-features = nix-command flakes";
     nix.enable = false;
     system.stateVersion = 6;
 
-    # Sudo with touch id
-    security.pam.services.sudo_local.touchIdAuth = true;
-
-    # User
+    # --- User & Shell Configuration ---
     system.primaryUser = "zhao";
     users.users.zhao = {
         name = "zhao";
         home = "/Users/zhao";
     };
 
-    # Shell
     programs.zsh.enable = true;
+
     environment = {
         shells = [ pkgs.zsh ];
         variables = {
@@ -26,23 +23,26 @@
         };
     };
 
-    # Keyboard settings
+    # --- Security & Input ---
+    security.pam.services.sudo_local.touchIdAuth = true;
+
     system.keyboard = {
         enableKeyMapping = true;
         remapCapsLockToEscape = true;
     };
 
-    # MacOS settings
+    # --- macOS System Defaults ---
     system.defaults = {
         dock = {
             autohide = true;
             mru-spaces = false;
-            wvous-tr-corner = 1;
-            wvous-tl-corner = 1;
-            wvous-br-corner = 1;
-            wvous-bl-corner = 1;
-            expose-group-apps = false;
             showMissionControlGestureEnabled = false;
+            expose-group-apps = false;
+        };
+
+        finder = {
+            AppleShowAllExtensions = true;
+            QuitMenuItem = true;
         };
 
         trackpad = {
@@ -52,40 +52,29 @@
             TrackpadRightClick = true;
         };
 
-        finder = {
-            AppleShowAllExtensions = true;
-            QuitMenuItem = true;
-        };
-
-        WindowManager = {
-            GloballyEnabled = false;
-            EnableStandardClickToShowDesktop = false;
-            EnableTilingByEdgeDrag = false;
-            EnableTopTilingByEdgeDrag = false;
-            EnableTiledWindowMargins = false;
-            EnableTilingOptionAccelerator = false;
-        };
-
-        CustomUserPreferences = {
-            "com.apple.spaces".spans-displays = false;
-            "com.apple.dock".workspaces-auto-swoosh = false;
-        };
-
         NSGlobalDomain = {
             AppleShowAllFiles = true;
             AppleShowAllExtensions = true;
             AppleSpacesSwitchOnActivate = false;
             AppleWindowTabbingMode = "always";
+
             InitialKeyRepeat = 15;
             KeyRepeat = 1;
+
+            "com.apple.swipescrolldirection" = true;
+
             NSAutomaticSpellingCorrectionEnabled = false;
             NSAutomaticCapitalizationEnabled = false;
             NSAutomaticQuoteSubstitutionEnabled = false;
             NSAutomaticDashSubstitutionEnabled = false;
             NSAutomaticPeriodSubstitutionEnabled = false;
             NSAutomaticWindowAnimationsEnabled = false;
-            "com.apple.springing.enabled" = false;
-            "com.apple.swipescrolldirection" = true;
+        };
+
+        WindowManager = {
+            GloballyEnabled = false;
+            EnableStandardClickToShowDesktop = false;
+            EnableTilingByEdgeDrag = false;
         };
     };
 }
