fa026b9fe60170b8f986e27b10a2e633de7240bd
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-03 14:16:20 -0500

    dbus!

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
     home.stateVersion = "22.11";
     home.file = { ".hushlogin".text = ""; };
 
     home.sessionVariables = {
         EDITOR = "nvim";
         SHELL_SESSIONS_DISABLE = "1";
-        DBUS_SESSION_BUS_ADDRESS = "unix:path=/Users/zhao/.cache/dbus-session-socket";
+        DBUS_SESSION_BUS_ADDRESS = "unix:path=${config.home.homeDirectory}/.cache/dbus-session-socket";
     };
 
     home.packages = with pkgs; [
@@ -54,14 +54,22 @@
     launchd.agents.dbus = {
         enable = true;
         config = {
+            Label = "org.nix-community.home.dbus";
             ProgramArguments = [
                 "${pkgs.dbus}/bin/dbus-daemon"
-                "--session"
-                "--address=unix:path=${config.home.homeDirectory}/.cache/dbus-session-socket"
                 "--nofork"
+                "--config-file=${pkgs.dbus}/share/dbus-1/session.conf"
+                "--address=unix:path=${config.home.homeDirectory}/.cache/dbus-session-socket"
             ];
             KeepAlive = true;
             ProcessType = "Interactive";
+            StandardOutPath = "${config.home.homeDirectory}/.cache/dbus.log";
+            StandardErrorPath = "${config.home.homeDirectory}/.cache/dbus.err";
         };
     };
+
+    home.activation.initDbus = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.cache"
+        $DRY_RUN_CMD ${pkgs.dbus}/bin/dbus-uuidgen --ensure
+    '';
 }
