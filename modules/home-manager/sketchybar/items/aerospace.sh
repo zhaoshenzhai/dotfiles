6b80782c79b2fa0adb72d90afb3a708bdb69886f
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-04 16:54:01 -0500

    a

--- modules/home-manager/sketchybar/items/aerospace.sh
+++ modules/home-manager/sketchybar/items/aerospace.sh
@@ -2,19 +2,19 @@
 
 sketchybar --add event aerospace_workspace_change
 for sid in $(aerospace list-workspaces --all); do
-    sketchybar --add item "space.$sid" left                            \
-              --set "space.$sid" label="$sid"                          \
-                                 label.padding_left=10                 \
-                                 label.padding_right=10                \
-                                 icon.padding_left=0                   \
-                                 icon.padding_right=0                  \
-                                 background.color=$BAR_COLOR           \
-                                 background.border_width=1             \
-                                 background.border_color=$BORDER_COLOR \
-                                 background.corner_radius=9            \
-                                 background.padding_right=2            \
-                                 background.padding_left=2             \
-               click_script="aerospace workspace $sid"                 \
+    sketchybar --add item "space.$sid" left                             \
+               --set "space.$sid" label="$sid"                          \
+                                  label.padding_left=10                 \
+                                  label.padding_right=10                \
+                                  icon.padding_left=0                   \
+                                  icon.padding_right=0                  \
+                                  background.color=$BAR_COLOR           \
+                                  background.border_width=1             \
+                                  background.border_color=$BORDER_COLOR \
+                                  background.corner_radius=9            \
+                                  background.padding_right=2            \
+                                  background.padding_left=2             \
+               click_script="aerospace workspace $sid"                  \
                script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
 
