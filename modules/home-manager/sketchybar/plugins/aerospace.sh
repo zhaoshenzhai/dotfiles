421ea4765f1b3dfb908d24ede6dfbe4e09da6a49
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-03 23:00:14 -0500

    bar

--- modules/home-manager/sketchybar/plugins/aerospace.sh
+++ modules/home-manager/sketchybar/plugins/aerospace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source "$HOME/.config/sketchybar/colors.sh"
+
 # If we didn't get the focused workspace from the event, ask Aerospace for it
 if [ -z "$FOCUSED_WORKSPACE" ]; then
     FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
@@ -11,11 +13,11 @@ HAS_WINDOWS=$(aerospace list-windows --workspace "$1")
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     # Case 1: It's the focused workspace. ALWAYS show it.
-    sketchybar --set $NAME label.drawing=on background.drawing=on label.color=$WHITE icon.color=$WHITE
+    sketchybar --set $NAME drawing=on label.drawing=on background.drawing=on label.color=$WHITE icon.color=$WHITE
 elif [ -n "$HAS_WINDOWS" ]; then
     # Case 2: It's NOT focused, but it HAS windows. Show it (but no background highlight).
-    sketchybar --set $NAME label.drawing=on background.drawing=off label.color=$GRAY icon.color=$GRAY
+    sketchybar --set $NAME drawing=on label.drawing=on background.drawing=off label.color=$GRAY icon.color=$GRAY
 else
     # Case 3: It's NOT focused and EMPTY. Hide it.
-    sketchybar --set $NAME label.drawing=off background.drawing=off
+    sketchybar --set $NAME drawing=off
 fi
