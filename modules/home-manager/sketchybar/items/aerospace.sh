aa55502caa38e83578cbd4b8dfbd1bf624b18935
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-03 22:27:48 -0500

    bar

--- modules/home-manager/sketchybar/items/aerospace.sh
+++ modules/home-manager/sketchybar/items/aerospace.sh
@@ -2,15 +2,21 @@
 
 sketchybar --add event aerospace_workspace_change
 for sid in $(aerospace list-workspaces --all); do
-    sketchybar --add item space."$sid" left \
-        --subscribe space."$sid" aerospace_workspace_change \
-        --set space."$sid" \
-        background.color=0x44ffffff \
-        background.corner_radius=5 \
-        background.height=20 \
-        background.drawing=off \
-        label.font.size=14.0 \
-        label="$sid" \
-        click_script="aerospace workspace $sid" \
-        script="$PLUGIN_DIR/aerospace.sh $sid"
+    sketchybar --add item "space.$sid" left                            \
+               --subscribe "space.$sid" aerospace_workspace_change     \
+                                        front_app_switched             \
+               --set "space.$sid" label="$sid"                         \
+                                  label.drawing=off                    \
+                                  background.drawing=off               \
+                                  # label.padding_right=33               \
+                                  # icon.padding_left=10                 \
+                                  # icon.padding_right=10                \
+                                  # background.color=$BAR_COLOR          \
+                                  # background.border_width=1            \
+                                  # background.border_color=0xff444444   \
+                                  # background.corner_radius=9           \
+                                  # background.padding_right=5           \
+                                  # background.padding_left=5            \
+               click_script="aerospace workspace $sid"                 \
+               script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
--- modules/home-manager/sketchybar/plugins/aerospace.sh
+++ modules/home-manager/sketchybar/plugins/aerospace.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# $1 is the workspace ID (passed from items/spaces.sh)
-source "$CONFIG_DIR/colors.sh"
-
 # If we didn't get the focused workspace from the event, ask Aerospace for it
 if [ -z "$FOCUSED_WORKSPACE" ]; then
     FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
@@ -14,10 +11,10 @@ HAS_WINDOWS=$(aerospace list-windows --workspace "$1")
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     # Case 1: It's the focused workspace. ALWAYS show it.
-    sketchybar --set $NAME drawing=on background.drawing=on label.color=$WHITE icon.color=$WHITE
+    sketchybar --set $NAME drawing.on label.drawing=on background.drawing=on label.color=$WHITE icon.color=$WHITE
 elif [ -n "$HAS_WINDOWS" ]; then
     # Case 2: It's NOT focused, but it HAS windows. Show it (but no background highlight).
-    sketchybar --set $NAME drawing=on background.drawing=off label.color=$GRAY icon.color=$GRAY
+    sketchybar --set $NAME drawing.on label.drawing=on background.drawing=off label.color=$GRAY icon.color=$GRAY
 else
     # Case 3: It's NOT focused and EMPTY. Hide it.
     sketchybar --set $NAME drawing=off
