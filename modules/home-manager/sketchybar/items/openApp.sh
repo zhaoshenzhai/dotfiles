17ebb5c0224367e1b36bf6d30f6a57ea8a0d8564
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-04 16:18:01 -0500

    fonts

--- modules/home-manager/sketchybar/items/openApp.sh
+++ modules/home-manager/sketchybar/items/openApp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sketchybar --add item openApp left                                         \
-           --set openApp      icon.font="sketchybar-app-font:Regular:16.0" \
-                              script="$PLUGIN_DIR/openApp.sh"              \
+sketchybar --add item openApp left                       \
+           --set openApp script="$PLUGIN_DIR/openApp.sh" \
            --subscribe openApp front_app_switched
--- modules/home-manager/sketchybar/plugins/openApp.sh
+++ modules/home-manager/sketchybar/plugins/openApp.sh
@@ -6,10 +6,10 @@ if [ "$SENDER" = "front_app_switched" ]; then
     fi
 
     ICON="$($HOME/.config/sketchybar/plugins/icon_map.sh "$INFO")"
-    if [[ $ICON = :* ]]; then
-        sketchybar --set $NAME label="$INFO" icon="$ICON"
-    else
-        sketchybar --set $NAME label="$INFO" icon="$ICON" icon.font="Font Awesome 7 Free:Solid:14.0"
-    fi
 
+    if [[ "$ICON" = :* ]]; then
+        sketchybar --set $NAME label="$INFO" icon=$ICON icon.font="sketchybar-app-font:Regular:16.0"
+    else
+        sketchybar --set $NAME label="$INFO" icon=$ICON icon.font="Font Awesome 7 Free:Solid:16.0"
+    fi
 fi
