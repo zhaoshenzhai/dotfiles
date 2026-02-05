c3339ac0fe4400810e824088ef6982dbf38655a1
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-05 11:08:35 -0500

    offset

--- modules/home-manager/sketchybar/items/apple.sh
+++ modules/home-manager/sketchybar/items/apple.sh
@@ -3,6 +3,7 @@
 sketchybar --add item apple left                      \
            --set apple  icon=􀣺                        \
                         icon.font.size=17             \
+                        icon.y_offset=3               \
                         icon.padding_left=10          \
                         icon.padding_right=10         \
                         label.padding_left=0          \
