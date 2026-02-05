67a5ab212ebe5fce47a0fa0301e80423d220a2b4
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-05 16:05:29 -0500

    position

--- modules/home-manager/sketchybar/items/apple.sh
+++ modules/home-manager/sketchybar/items/apple.sh
@@ -4,7 +4,7 @@ sketchybar --add item apple left                      \
            --set apple  icon=􀣺                        \
                         icon.font.size=17             \
                         icon.y_offset=3               \
-                        icon.padding_left=10          \
+                        icon.padding_left=15          \
                         icon.padding_right=10         \
                         label.padding_left=0          \
                         label.padding_right=0         \
