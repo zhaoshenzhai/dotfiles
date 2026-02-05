2d7461069eb422b3b5ec2e38d75cbca3ffd9c3e5
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-04 19:49:19 -0500

    more optimizations

--- modules/home-manager/sketchybar/plugins/openApp.sh
+++ modules/home-manager/sketchybar/plugins/openApp.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+source "$HOME/.config/sketchybar/plugins/icon_map.sh" > /dev/null
 
 if [ "$SENDER" = "front_app_switched" ]; then
     if [ "$INFO" = ".zathura-wrapped" ]; then
         INFO=zathura
     fi
 
-    ICON="$($HOME/.config/sketchybar/plugins/icon_map.sh "$INFO")"
-    sketchybar --set $NAME label="$INFO" icon=$ICON icon.font="sketchybar-app-font:Regular:16.0"
+    icon_map "$INFO"
+    sketchybar --set "$NAME" label="$INFO" icon="$icon_result" icon.font="sketchybar-app-font:Regular:16.0"
 fi
