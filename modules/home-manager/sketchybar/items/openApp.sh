22a1c532429c2fe50519868d023cd9eae6e12479
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-13 11:41:55 -0500

    openApp

--- modules/home-manager/sketchybar/items/openApp.sh
+++ modules/home-manager/sketchybar/items/openApp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sketchybar --add item openApp right                      \
+sketchybar --add item openApp left                       \
            --set openApp script="$PLUGIN_DIR/openApp.sh" \
            --subscribe openApp front_app_switched
