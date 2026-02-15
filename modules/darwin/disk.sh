6a1412758fd6e9de4025ea185bce196fe53a8f92
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-15 17:37:24 -0500

    vifm

--- modules/darwin/disk.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-avail=$(df --output=avail | sed -n 2p | awk '{$1=$1};1')
-used=$(df --output=used | sed -n 2p | awk '{$1=$1};1')
-percent=$(echo "scale=3; $used/($used+$avail)" | bc | sed 's/.//' | sed 's/./&./2' | sed 's/^0*//')
-
-printf "%s %s" "<fn=1></fn><hspace=5/>" "$percent%"
--- modules/home-manager/vifm.nix
+++ modules/home-manager/vifm.nix
@@ -30,31 +30,31 @@
             nnoremap l :file<cr><cr>
             nnoremap t :!alacritty --working-directory %d &<cr>
             nnoremap s :!alacritty --title vifm-float --option "window.dimensions={columns=100,lines=35}" --option "window.position={x=525,y=250}" --working-directory %d &<cr>
-            nnoremap c :!/etc/profiles/per-user/zhao/bin/alacritty --title vifm-float --option "window.dimensions={columns=100,lines=35}" --option "window.position={x=525,y=250}" --working-directory %d -e zsh -ic "~/iCloud/Dotfiles/modules/darwin/compress.sh *.pdf"
+            nnoremap c :!~/iCloud/Dotfiles/modules/darwin/compress.sh *.pdf >/dev/null 2>&1 &
             
             filetype *.pdf zathura %c >/dev/null 2>&1 &
             filetype *.jpg,*.jpeg,*.png,*.gif open %c &
 
             highlight clear
-            highlight Border	cterm=none	ctermfg=035	ctermbg=default
-            highlight TopLine	cterm=none	ctermfg=002	ctermbg=default
-            highlight TopLineSel cterm=bold ctermfg=002 ctermbg=default
-            highlight Win		cterm=none	ctermfg=250	ctermbg=default
-            highlight Directory	cterm=bold	ctermfg=004	ctermbg=default
-            highlight CurrLine	cterm=bold,inverse	ctermfg=default	ctermbg=default
-            highlight OtherLine	cterm=bold	ctermfg=default	ctermbg=default
-            highlight Selected	cterm=none	ctermfg=003	ctermbg=008
-            highlight JobLine	cterm=bold	ctermfg=250	ctermbg=008
-            highlight StatusLine cterm=none ctermfg=250 ctermbg=default
-            highlight ErrorMsg	cterm=bold	ctermfg=001	ctermbg=default
-            highlight WildMenu	cterm=bold	ctermfg=015	ctermbg=250
-            highlight CmdLine	cterm=none	ctermfg=007	ctermbg=default
-            highlight Executable	cterm=bold	ctermfg=002	ctermbg=default
-            highlight Link		cterm=bold	ctermfg=006	ctermbg=default
-            highlight BrokenLink	cterm=bold	ctermfg=001	ctermbg=default
-            highlight Device	cterm=bold,standout	ctermfg=000	ctermbg=011
-            highlight Fifo		cterm=none	ctermfg=003	ctermbg=default
-            highlight Socket	cterm=bold	ctermfg=005	ctermbg=default
+            highlight Border	 cterm=none	         ctermfg=035     ctermbg=default
+            highlight TopLine	 cterm=none	         ctermfg=002     ctermbg=default
+            highlight TopLineSel cterm=bold          ctermfg=002     ctermbg=default
+            highlight Win        cterm=none	         ctermfg=250     ctermbg=default
+            highlight Directory	 cterm=bold	         ctermfg=004     ctermbg=default
+            highlight CurrLine   cterm=bold,inverse	 ctermfg=default ctermbg=default
+            highlight OtherLine  cterm=bold	         ctermfg=default ctermbg=default
+            highlight Selected   cterm=none	         ctermfg=003     ctermbg=008
+            highlight JobLine    cterm=bold	         ctermfg=250     ctermbg=008
+            highlight StatusLine cterm=none          ctermfg=250     ctermbg=default
+            highlight ErrorMsg   cterm=bold	         ctermfg=001     ctermbg=default
+            highlight WildMenu   cterm=bold	         ctermfg=015     ctermbg=250
+            highlight CmdLine    cterm=none	         ctermfg=007     ctermbg=default
+            highlight Executable cterm=bold	         ctermfg=002     ctermbg=default
+            highlight Link       cterm=bold	         ctermfg=006     ctermbg=default
+            highlight BrokenLink cterm=bold	         ctermfg=001     ctermbg=default
+            highlight Device     cterm=bold,standout ctermfg=000     ctermbg=011
+            highlight Fifo       cterm=none	         ctermfg=003     ctermbg=default
+            highlight Socket     cterm=bold	         ctermfg=005     ctermbg=default
         '';
     };
 }
