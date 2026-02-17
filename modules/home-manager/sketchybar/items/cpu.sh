a7b827b4a00389b01b3ac7ddb415b34e95372a9e
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-17 18:03:28 -0500

    cpu

--- modules/home-manager/sketchybar/items/cpu.sh
+++ modules/home-manager/sketchybar/items/cpu.sh
@@ -4,19 +4,25 @@ source "$HOME/.config/sketchybar/colors.sh"
 
 sketchybar --add graph cpu right 50                \
            --set cpu   update_freq=1               \
+                       y_offset=2                  \
                        graph.color=$WHITE          \
-                       graph.fill_color=0x55abb2bf \
+                       graph.fill_color=$WHITE_    \
                        graph.line_width=1          \
-                       icon.pading_left=0          \
-                       icon.pading_right=0         \
-                       label.pading_left=0         \
-                       label.pading_right=0        \
-                       background.padding_left=0   \
-                       background.padding_right=0  \
+                       icon.padding_left=5         \
+                       icon.padding_right=0        \
+                       label.padding_left=0        \
+                       label.padding_right=7       \
+                       background.y_offset=-2      \
+                       background.padding_left=1   \
+                       background.padding_right=1  \
                        script="$PLUGIN_DIR/cpu.sh"
 
-sketchybar --add item cpu.label right             \
-           --set cpu.label icon=􀫥                 \
-                           width=0                \
-                           padding_right=-60      \
+sketchybar --add item cpu.label right                      \
+           --set cpu.label icon=􀫥                          \
+                           icon.shadow.drawing=on          \
+                           icon.shadow.color=$SHADOW_COLOR \
+                           icon.shadow.angle=90            \
+                           icon.shadow.distance=2          \
+                           blur_radius=10                  \
+                           padding_right=-45               \
                            background.drawing=off
--- modules/home-manager/sketchybar/plugins/cpu.sh
+++ modules/home-manager/sketchybar/plugins/cpu.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
+source "$HOME/.config/sketchybar/colors.sh"
 CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
 
-calculate_cpu() {
-    ps -A -o %cpu | awk -v cores="$CORE_COUNT" '{s+=$1} END {printf "%.3f", s/cores/100}'
+update_cpu() {
+    CPU=$(ps -A -o %cpu | awk -v cores="$CORE_COUNT" '{s+=$1} END {printf "%.3f", s/cores/100}')
+
+    if [ "$(echo "$CPU" | awk '{print ($1 > 0.4 ? 1 : 0)}')" -eq 1 ]; then
+        sketchybar --set cpu graph.color=$RED graph.fill_color=$RED_
+        sketchybar --set cpu.label icon.color=$RED
+    elif [ "$(echo "$CPU" | awk '{print ($1 > 0.2 ? 1 : 0)}')" -eq 1 ]; then
+        sketchybar --set cpu graph.color=$ORANGE graph.fill_color=$ORANGE_
+        sketchybar --set cpu.label icon.color=$ORANGE
+    else
+        sketchybar --set cpu graph.color=$WHITE graph.fill_color=$WHITE_
+        sketchybar --set cpu.label icon.color=$WHITE
+    fi
+
+    sketchybar --push cpu "$CPU"
 }
 
-sketchybar --push cpu "$(calculate_cpu)"
+update_cpu
 sleep 0.5
-sketchybar --push cpu "$(calculate_cpu)"
+update_cpu
