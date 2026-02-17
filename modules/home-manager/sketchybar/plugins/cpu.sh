191c1b5196faa60a048c6694a5f85e56378e894a
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-17 18:40:42 -0500

    cpu

--- modules/home-manager/sketchybar/plugins/cpu.sh
+++ modules/home-manager/sketchybar/plugins/cpu.sh
@@ -9,7 +9,7 @@ update_cpu() {
     if [ "$(echo "$CPU" | awk '{print ($1 > 0.4 ? 1 : 0)}')" -eq 1 ]; then
         sketchybar --set cpu graph.color=$RED graph.fill_color=$RED_
         sketchybar --set cpu.label icon.color=$RED
-    elif [ "$(echo "$CPU" | awk '{print ($1 > 0.2 ? 1 : 0)}')" -eq 1 ]; then
+    elif [ "$(echo "$CPU" | awk '{print ($1 > 0.1 ? 1 : 0)}')" -eq 1 ]; then
         sketchybar --set cpu graph.color=$ORANGE graph.fill_color=$ORANGE_
         sketchybar --set cpu.label icon.color=$ORANGE
     else
