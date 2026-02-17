a48baeca80966685b8b32a649f4a4c7421ea2680
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-17 17:38:38 -0500

    cpu

--- modules/home-manager/sketchybar/items/cpu.sh
+++ modules/home-manager/sketchybar/items/cpu.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-sketchybar --add item cpu right                   \
-           --set cpu  icon=􀫥                      \
-                      update_freq=2               \
-                      script="$PLUGIN_DIR/cpu.sh"
+source "$HOME/.config/sketchybar/colors.sh"
+
+sketchybar --add graph cpu right 50                \
+           --set cpu   update_freq=1               \
+                       graph.color=$WHITE          \
+                       graph.fill_color=0x55abb2bf \
+                       graph.line_width=1          \
+                       icon.pading_left=0          \
+                       icon.pading_right=0         \
+                       label.pading_left=0         \
+                       label.pading_right=0        \
+                       background.padding_left=0   \
+                       background.padding_right=0  \
+                       script="$PLUGIN_DIR/cpu.sh"
+
+sketchybar --add item cpu.label right             \
+           --set cpu.label icon=􀫥                 \
+                           width=0                \
+                           padding_right=-60      \
+                           background.drawing=off
--- modules/home-manager/sketchybar/plugins/cpu.sh
+++ modules/home-manager/sketchybar/plugins/cpu.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
-CPU_PERCENT=$(ps -A -o %cpu | awk -v cores="$CORE_COUNT" '{s+=$1} END {printf "%.0f", s/cores}')
 
-sketchybar --animate tanh 8 --set "$NAME" label="$CPU_PERCENT%"
+calculate_cpu() {
+    ps -A -o %cpu | awk -v cores="$CORE_COUNT" '{s+=$1} END {printf "%.3f", s/cores/100}'
+}
+
+sketchybar --push cpu "$(calculate_cpu)"
+sleep 0.5
+sketchybar --push cpu "$(calculate_cpu)"
