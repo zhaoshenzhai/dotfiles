4e5a45f054be620a7fe732dbfaaeb42c65ca126f
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-02-19 17:07:05 -0500

    nixd

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -12,10 +12,12 @@
             ls = "ls --color=auto -F";
             la = "ls -lhAr --color=auto -F";
             tree = "tree -C";
-            nixs = "sudo darwin-rebuild switch --flake ~/iCloud/Dotfiles#puppy; aerospace reload-config; sketchybar --reload";
-            nixu = "pushd ~/iCloud/Dotfiles; nix flake update; nixs; popd";
             zoom = "open -a zoom.us";
             skim = "open -a Skim";
+
+            nixs = "sudo darwin-rebuild switch --flake ~/iCloud/Dotfiles#puppy; aerospace reload-config; sketchybar --reload";
+            nixu = "pushd ~/iCloud/Dotfiles; nix flake update; nixs; popd";
+            nixd = "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old; nix-collect-garbage -d; nixs";
         };
 
         initContent = ''
