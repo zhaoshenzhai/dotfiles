da713b3b958b385edac0e970cec017cff3b9943b
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-03-19 22:44:29 -0400

    mpv

--- flake.nix
+++ flake.nix
@@ -10,19 +10,35 @@
 
         nixvim.url = "github:nix-community/nixvim";
         nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+        yt-dlp-master = { url = "github:yt-dlp/yt-dlp"; flake = false; };
     };
 
     outputs = inputs@{ nixpkgs, home-manager, darwin, nixvim, ... }: {
         darwinConfigurations.puppy = darwin.lib.darwinSystem {
+            pkgs = import nixpkgs {
+                system = "aarch64-darwin";
+                overlays = [
+                    (final: prev: {
+                        yt-dlp = prev.yt-dlp.overrideAttrs (oldAttrs: {
+                            version = "nightly-${inputs.yt-dlp-master.shortRev or "dirty"}";
+                            src = inputs.yt-dlp-master;
+                        });
+                    })
+                ];
+            };
+
             system = "aarch64-darwin";
-            pkgs = import nixpkgs { system = "aarch64-darwin"; };
+            specialArgs = { inherit inputs; };
 
             modules = [
                 ./modules/darwin
                 home-manager.darwinModules.home-manager {
                     home-manager = {
                         useGlobalPkgs = true;
-                        useUserPackages = true;           
+                        useUserPackages = true;
+                        extraSpecialArgs = { inherit inputs; };
+
                         users.zhao = {
                             imports = [
                                 ./modules/home-manager
@@ -32,6 +48,6 @@
                     };
                 }
             ];
-        }; 
+        };
     };
 }
