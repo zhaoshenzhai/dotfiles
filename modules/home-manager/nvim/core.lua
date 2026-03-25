554b39bd334882f95b179f03c0cc7cfcaaff5d79
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-03-25 14:46:20 -0400

    automatically remove trailing whitespaces

--- modules/home-manager/nvim/core.lua
+++ modules/home-manager/nvim/core.lua
@@ -21,3 +21,12 @@ vim.api.nvim_create_autocmd({"VimLeave", "VimSuspend"}, {
         )
     end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        local save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
