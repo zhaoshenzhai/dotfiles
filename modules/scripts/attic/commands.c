5d096c1269c108b07b523387fffb68b956cb16d2
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-04-05 17:25:27 -0400

    sleep

--- modules/scripts/attic/commands.c
+++ modules/scripts/attic/commands.c
@@ -527,6 +527,5 @@ void launchGraph(void) {
     system(cmd);
 
     system("osascript -e 'tell application \"System Events\" to set visible of front process to false'");
-    usleep(100000);
     exit(0);
 }
