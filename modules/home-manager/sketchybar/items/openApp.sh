93b4edd9b2eacb82110a2b94e148bc8f481f491a
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-04-23 14:30:51 -0400

    kind of works now

--- modules/home-manager/sketchybar/items/openApp.sh
+++ modules/home-manager/sketchybar/items/openApp.sh
@@ -7,4 +7,4 @@ sleep 0.2
 sketchybar --add event aerospace_custom_app_switched
 sketchybar --add item openApp left                         \
            --set openApp mach_helper="openApp_plugin_mach" \
-           --subscribe openApp aerospace_custom_app_switched
+           --subscribe openApp aerospace_custom_app_switched front_app_switched
--- modules/home-manager/sketchybar/src/openApp_plugin.c
+++ modules/home-manager/sketchybar/src/openApp_plugin.c
@@ -4,29 +4,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 void handler(env env) {
     char* name = env_get_value_for_key(env, "NAME");
     char* sender = env_get_value_for_key(env, "SENDER");
     char* info = env_get_value_for_key(env, "INFO");
     char* title = env_get_value_for_key(env, "TITLE");
-    char* workspace = env_get_value_for_key(env, "AEROSPACE_FOCUSED_WORKSPACE");
 
-    if (name[0] == '\0' || sender[0] == '\0' || strcmp(sender, "aerospace_custom_app_switched") != 0 || info[0] == '\0') { return; }
+    if (name[0] == '\0' || sender[0] == '\0' || info[0] == '\0') { return; }
 
-    if (title[0] != '\0' && strcasecmp(info, "alacritty") == 0) {
-        if (strcmp(title, "launcher") == 0) { return; }
-        else if (strcmp(title, "alacritty-float") == 0) { info = "alacritty"; }
-        else if (strcmp(title, "vifm") == 0) { info = "vifm"; }
-        else if (strcmp(title, "vifm-float") == 0) { info = "vifm"; }
-        else if (strcmp(title, "attic") == 0) { info = "attic"; }
-        else if (strcmp(title, "btop") == 0) { info = "btop"; }
-        else if (strcmp(title, "nvim") == 0) { info = "nvim"; }
-        else if (strcmp(title, "git") == 0) { info = "git"; }
+    bool is_aerospace = (strcmp(sender, "aerospace_custom_app_switched") == 0);
+    bool is_front_app = (strcmp(sender, "front_app_switched") == 0);
+
+    if (!is_aerospace && !is_front_app) { return; }
+
+    if (is_front_app) {
+        if (strcasecmp(info, "puppy") != 0) return;
+    } else if (is_aerospace) {
+        if (title[0] != '\0' && strcasecmp(info, "alacritty") == 0) {
+            if (strcmp(title, "launcher") == 0) { return; }
+            else if (strcmp(title, "alacritty-float") == 0) { info = "alacritty"; }
+            else if (strcmp(title, "vifm") == 0) { info = "vifm"; }
+            else if (strcmp(title, "vifm-float") == 0) { info = "vifm"; }
+            else if (strcmp(title, "attic") == 0) { info = "attic"; }
+            else if (strcmp(title, "btop") == 0) { info = "btop"; }
+            else if (strcmp(title, "nvim") == 0) { info = "nvim"; }
+            else if (strcmp(title, "git") == 0) { info = "git"; }
+        }
+
+        if (title[0] != '\0' && strcasecmp(info, "attic-graph") == 0) { info = "attic"; }
     }
 
-    if (title[0] != '\0' && strcasecmp(info, "attic-graph") == 0) { info = "attic"; }
-    if (title[0] != '\0' && strcasecmp(info, "puppy") == 0) { info = "puppy"; }
+    if (strcasecmp(info, "puppy") == 0) { info = "puppy"; }
 
     const char* icon = get_icon_for_app(info);
 
