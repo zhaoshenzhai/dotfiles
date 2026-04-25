aade556d318e093e0668f3669f71e29199bb5a7e
Zhaoshen Zhai <zhaoshen.zhai@gmail.com>
2026-04-25 15:03:52 -0400

    reorganizing

--- modules/home-manager/zsh/init.sh
+++ modules/home-manager/zsh/init.sh
@@ -2,18 +2,21 @@ export FF_LINES_UP=16
 export FF_COLUMN=80
 export FF_WIDTH=60
 
-renderCalendar() {
-    local cal_file="$HOME/.cache/fastfetch/myCalendar"
-    if [[ -f "$cal_file" ]]; then
-        printf "\e[s"
-        printf "\e[${FF_LINES_UP}A"
-
-        while IFS= read -r line; do
-            printf "\e[${FF_COLUMN}G%s\n" "$line"
-        done < "$cal_file"
-
-        printf "\e[u"
+hideTTY() {
+    if [[ -t 0 ]]; then
+        stty -echo 2>/dev/null
     fi
+}
+
+restoreTTY() {
+    autoload -Uz add-zle-hook-widget
+    function _restore_tty_echo() {
+        if [[ -t 0 ]]; then
+            stty echo 2>/dev/null
+        fi
+        add-zle-hook-widget -d line-init _restore_tty_echo
+    }
+    add-zle-hook-widget line-init _restore_tty_echo
 }
 
 setupCompletions() {
@@ -36,43 +39,46 @@ setupKeybinds() {
     bindkey '^x' delete-char
 }
 
+positionCalendar() {
+    local cal_file="$HOME/.cache/fastfetch/myCalendar"
+    if [[ -f "$cal_file" ]]; then
+        printf "\e[s"
+        printf "\e[${FF_LINES_UP}A"
+
+        while IFS= read -r line; do
+            printf "\e[${FF_COLUMN}G%s\n" "$line"
+        done < "$cal_file"
+
+        printf "\e[u"
+    fi
+}
+
+updateCalendar() {
+    env -u DYLD_INSERT_LIBRARIES icalBuddy -sc -b "• " -ab "❗️ " eventsToday uncompletedTasks 2>/dev/null | \
+        fold -w ${FF_WIDTH} -s > ~/.cache/fastfetch/myCalendar.tmp
+    mv ~/.cache/fastfetch/myCalendar.tmp ~/.cache/fastfetch/myCalendar
+}
+
 updateFetch() {
-    cache_ff() {
+    cacheFF() {
         local module=$1
         local target="$HOME/.cache/fastfetch/$2"
         fastfetch --config none --structure "$module" --logo none | sed 's/^[^:]*: //' > "$target.tmp"
         mv "$target.tmp" "$target"
     }
 
-    cache_ff "wm" "myWM"
-    cache_ff "packages" "myPackages"
-    cache_ff "terminal" "myTerminal"
-    cache_ff "shell" "myShell"
-    cache_ff "editor" "myEditor"
-
-    env -u DYLD_INSERT_LIBRARIES icalBuddy -sc -b "• " -ab "❗️ " eventsToday uncompletedTasks 2>/dev/null | \
-        fold -w ${FF_WIDTH} -s > ~/.cache/fastfetch/myCalendar.tmp
-    mv ~/.cache/fastfetch/myCalendar.tmp ~/.cache/fastfetch/myCalendar
+    cacheFF "wm" "myWM"
+    cacheFF "packages" "myPackages"
+    cacheFF "terminal" "myTerminal"
+    cacheFF "shell" "myShell"
+    cacheFF "editor" "myEditor"
 }
 
-restoreTTY() {
-    autoload -Uz add-zle-hook-widget
-    function _restore_tty_echo() {
-        if [[ -t 0 ]]; then
-            stty echo 2>/dev/null
-        fi
-        add-zle-hook-widget -d line-init _restore_tty_echo
-    }
-    add-zle-hook-widget line-init _restore_tty_echo
-}
-
-if [[ -t 0 ]]; then
-    stty -echo 2>/dev/null
-fi
-
+hideTTY
 fastfetch
-renderCalendar
+positionCalendar
 setupCompletions
 setupKeybinds
 updateFetch &!
+updateCalendar &!
 restoreTTY
